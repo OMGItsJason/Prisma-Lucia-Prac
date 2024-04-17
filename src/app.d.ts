b3f76876eed7e471e43fb50361883c21059873ae
Jason Chanliongco Maliquid <jasonm2822@gmail.com>
2024-04-17 08:18:29 +0800

    prisma lucia setup

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,6 +2,10 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Locals {
+			user: import('lucia').User | null;
+			session: import('lucia').Session | null;
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
