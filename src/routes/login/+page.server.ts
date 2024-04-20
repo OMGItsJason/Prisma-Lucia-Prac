139dd53a6c57b43d034b5f427301fae7cf3504ac
Jason Chanliongco Maliquid <jasonm2822@gmail.com>
2024-04-20 23:18:07 +0800

    🥳 Zod & Superform Test

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,30 +2,53 @@ import { Argon2id } from 'oslo/password';
 import { fail, redirect } from '@sveltejs/kit';
 import { lucia } from '$lib/server/auth';
 import { prisma } from '$lib/server/prisma';
-
 import type { Actions } from './$types';
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+
+const logInSchema = z.object({
+	username: z.string({ required_error: 'Invalid Email' }).min(1),
+	password: z.string({ required_error: 'Invalid Password' }).min(1)
+});
+
+export const load = async () => {
+	const form = await superValidate(zod(logInSchema));
+	return {
+		form
+	};
+};
 
 export const actions: Actions = {
 	default: async (event) => {
-		const formData = await event.request.formData();
-		const username = formData.get('username');
-		const password = formData.get('password');
+		const formData = await superValidate(event, zod(logInSchema));
 
-		if (typeof password !== 'string' || password.length < 3 || password.length > 255) {
+		if (!formData) {
 			return fail(400, {
-				message: 'Invalid password'
+				formData,
+				error: 'Invalid form data'
 			});
 		}
 
+		const { username, password } = formData.data;
+
 		const db = await prisma.user.findFirst({
 			where: {
-				username: username
+				username
 			}
 		});
 
 		if (!db) {
 			return fail(400, {
-				message: 'Invalid credentials'
+				formData,
+				message: 'Invalid Username'
+			});
+		}
+
+		if (typeof password !== 'string' || password.length < 3 || password.length > 255) {
+			return fail(400, {
+				formData,
+				message: 'Invalid Password'
 			});
 		}
 
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import { superForm } from 'sveltekit-superforms/client';
+	import type { PageData } from './$types';
+	import SuperDebug from 'sveltekit-superforms';
+
+	export let data: PageData;
+	const { form, errors, enhance } = superForm(data.form);
 </script>
 
+<SuperDebug data="$form" />
 <h1>Log In</h1>
 <form method="post" use:enhance>
 	<label for="username">Username</label>
-	<input name="username" id="username" /><br />
+	<input name="username" id="username" bind:value={$form.username} />
+	{#if $errors.username}
+		<p>{$errors.username}</p>
+	{/if}<br />
 	<label for="password">Password</label>
-	<input type="password" name="password" id="password" /><br />
+	<input type="password" name="password" id="password" bind:value={$form.password} />
+	{#if $errors.password}
+		<p>{$errors.password}</p>
+	{/if}<br />
 	<button>Continue</button>
 </form>
